9ca67f0169fc873a8a54e43a8b0750b3f4ee33fc
pablobidiaki <pablobidiaki@hotmail.com>
2022-12-21 11:19:32 -0300

    detalhes

--- destino/menuBurguer.js
+++ destino/menuBurguer.js
@@ -5,9 +5,11 @@ function menuBurguer(){
         document.getElementById('opcoes').classList.remove('none')
         document.getElementById('fechar').classList.remove('none')
         document.getElementById('burguer').classList.add('none')
+        document.querySelector('body').classList.add('over')
     }else{
         document.getElementById('opcoes').classList.add('none')
         document.getElementById('burguer').classList.remove('none')
         document.getElementById('fechar').classList.add('none')
+        document.querySelector('body').classList.remove('over')
     }
 }
